832bb0e77a4c507b27d3eb03035fa3465f14b2ec
lgthvinh <everwellmax@gmail.com>
2026-01-18 03:15:46 +0700

    Initialize `meo-android` library module
    - Create `meo-android` module with basic Android library structure and `MeoSdk` class.
    - Add `appcompat` and `material` dependencies to `libs.versions.toml`.
    - Configure `build.gradle.kts` for the new module, including local JAR dependencies for `applicationbase` and `meo-common`.
    - Register the `:meo-android` module in `settings.gradle.kts` and IntelliJ project settings.
    - Include default ProGuard rules, unit test, and instrumented test templates.

---
+++ meo-android/src/main/java/org/thingai/android/module/meo/MeoSdk.kt
@@ -0,0 +1,4 @@
+package org.thingai.android.module.meo
+
+class MeoSdk {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "AndroidMeo"
 include(":app")
- 
+include(":meo-android")
